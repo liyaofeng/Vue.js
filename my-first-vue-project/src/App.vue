f906c59d7af9f95ca812d6c1b3186ae414d58162
Gigaiot <916238589@qq.com>
2017-01-18 17:09:43 +0800

    first commit

---
+++ my-first-vue-project/src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div id="app">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li v-for="item in items" v-text="item.label" :class="{finished: item.isFinished}">
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'This is a todo list!',
+      items: [
+        {
+          label: "coding",
+          isFinished: false
+        },
+        {
+          label: "walking",
+          isFinished: true
+        }],
+      // finishedClass: "finished"
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+.finished {
+  text-decoration: underline;
+}
+
+body {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+}
+
+#app {
+  color: #2c3e50;
+  margin-top: -100px;
+  max-width: 600px;
+  font-family: Source Sans Pro, Helvetica, sans-serif;
+  text-align: center;
+}
+
+#app a {
+  color: #42b983;
+  text-decoration: none;
+}
+
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
